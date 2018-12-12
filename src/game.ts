fd8b4ca967733f70f66ccc84b969ba9f24ab32c2
Nico E <earnshaw.nico@gmail.com>
2018-12-12 12:03:42 -0300

    reorder for more clarity

--- src/game.ts
+++ src/game.ts
@@ -32,11 +32,8 @@ export class RotatorSystem implements ISystem {
   }
 }
 
-
-const doorMaterial = new Material()
-doorMaterial.albedoColor = Color3.Red()
-doorMaterial.metallic = 0.9
-doorMaterial.roughness = 0.1
+// Add system to engine
+engine.addSystem(new RotatorSystem())
 
 // Define fixed walls
 const wall1 = new Entity()
@@ -45,6 +42,7 @@ wall1.get(Transform).position.set(5.75, 1, 3)
 wall1.get(Transform).scale.set(1.5, 2, 0.05)
 wall1.set(new BoxShape())
 wall1.get(BoxShape).withCollisions = true
+engine.addEntity(wall1)
 
 const wall2 = new Entity()
 wall2.set(new Transform())
@@ -52,13 +50,13 @@ wall2.get(Transform).position.set(3.25, 1, 3)
 wall2.get(Transform).scale.set(1.5, 2, 0.05)
 wall2.set(new BoxShape())
 wall2.get(BoxShape).withCollisions = true
+engine.addEntity(wall2)
 
-
-// Define wrapper entity to rotate door. This is the entity that actually rotates.
-const doorPivot = new Entity()
-doorPivot.set(new Transform())
-doorPivot.get(Transform).position.set(4, 1, 3)
-doorPivot.set(new DoorState())
+// Define a material to color the door red
+const doorMaterial = new Material()
+doorMaterial.albedoColor = Color3.Red()
+doorMaterial.metallic = 0.9
+doorMaterial.roughness = 0.1
 
 // Add actual door to scene. This entity doesn't rotate, its parent drags it with it.
 const door = new Entity()
@@ -68,6 +66,19 @@ door.get(Transform).scale.set(1, 2, 0.05)
 door.set(new BoxShape())
 door.set(doorMaterial)
 door.get(BoxShape).withCollisions = true
+engine.addEntity(door)
+
+// Define wrapper entity to rotate door. This is the entity that actually rotates.
+const doorPivot = new Entity()
+doorPivot.set(new Transform())
+doorPivot.get(Transform).position.set(4, 1, 3)
+doorPivot.set(new DoorState())
+engine.addEntity(doorPivot)
+
+// Set the door as a child of doorPivot
+door.setParent(doorPivot)
+
+// Set the click behavior for the door
 door.set(
   new OnClick(_ => {
     let state = doorPivot.get(DoorState)
@@ -75,15 +86,6 @@ door.set(
   })
 )
 
-// Set the door as a child of doorPivot
-door.setParent(doorPivot)
 
-// Add all entities to engine
-engine.addEntity(wall1)
-engine.addEntity(wall2)
-engine.addEntity(doorPivot)
-engine.addEntity(door)
 
-// Add system to engine
-engine.addSystem(new RotatorSystem())
 
